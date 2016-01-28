5daf51affb2260d1f1d9fa6f6c14e59b8f87eee5
Nazar Shershin <nazar_sh@seven.ua>
2016-01-28 19:27:23 +0200

    v1.1 - Rect/ Images/ recources load

--- js/cherry.js
+++ js/cherry.js
@@ -7,6 +7,13 @@
 /*
  * Not editable params
  */
+var loading = false;
+window.requestAnimFrame = (function (callback) {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
 $.serviceOptions = {
     errorClass: 'error',
     type: ['field', 'popup', 'underfield'],
@@ -16,34 +23,101 @@ $.serviceOptions = {
     criticalError: {
         el_not_exist: 'Canvas element not exist',
         params_format: 'Wrong params. Please check documentation',
+        must_be_string: 'This parameter have to be string',
+        image_not_found: 'Image {path} not found'
     },
     validPrevImgClass: 'valid_prev_img',
     elementAttribute: {min: 'data-min', max: 'data-max', in: 'data-in', ajax: 'data-ajax', file: 'data-file'},
 }
 var cherry = $c = {
+    options: {
+        frame: true
+    },
+    images: {},
     canvas: false,
     frame_count: 0,
+    per_min: 0,
     lastTime: false,
-    init: function (selector, options, callback) {
-        var element = document.querySelector(selector);
+    init: function (selector, options, callback, game) {
+        var element = document.querySelector(selector),
+                $this = this;
         if (element)
         {
-            this.canvas = element.getContext('2d');
+            $this.canvas = element.getContext('2d');
+
             /* set options */
-            if (options.width !== 'undefined') {
-                this.canvas.width = options.width;
+            if (options.width !== undefined) {
+                $this.canvas.width = options.width;
                 //element.style.width = options.width + "px";
             }
-            if (options.height !== 'undefined') {
-                this.canvas.height = options.height;
+            if (options.height !== undefined) {
+                $this.canvas.height = options.height;
                 //element.style.height = options.height + "px";
             }
-            var canvas = this;
+            if (options.frame !== undefined) {
+                $this.options.frame = options.frame;
+            }
+            //
+            if (typeof game == 'object') {
+                $this.__proto__ = game;
+                if (game.resources != undefined && game.resources instanceof Object) {
+                    for (var i in game.resources) {
+                        _load(i, game.resources[i]);
+                    }
+                    function _load(url, path) {
+                        if ($this.images[url]) {
+                            return $this.images[url];
+                        } else {
+                            var img = new Image();
+                            img.src = path;
+                            img.onload = function () {
+                                $this.images[url] = img;
+                                if(ready())
+                                    animate($this) 
+                            };
+                            $this.images[url] = false;
+                        }
+                    }
+                    function ready(){
+                        for (var i in game.resources) {
+                            if(!$this.images[i])
+                                return false;
+                        }
+                        return true;
+                    }
+                }
+            }
+            
             /* return object*/
-            setInterval(function(){ 
-                canvas.canvas.clearRect(0,0, canvas.canvas.width, canvas.canvas.height )
-                return callback(canvas) 
-            }, 1000/25);
+            var lastTime,
+                    last_show = 0,
+                    lastFrame,
+                    per_min = 0;
+            function animate($this) {
+                requestAnimFrame(function () {
+                    var now = Date.now();
+                    /*clear place*/
+                    $this.canvas.clearRect(0, 0, $this.canvas.width, $this.canvas.height);
+                    $this.frame_count++;
+                    lastTime = now;
+
+
+                    if (now > last_show + 1000) {
+                        last_show = now;
+                        per_min = $this.frame_count - lastFrame;
+                        lastFrame = $this.frame_count;
+                    }
+                    /* render callback*/
+                    callback($this);
+                    /* end - callback*/
+                    if (per_min)
+                        $this.text('Frames per sec: ' + per_min, {position: [20, 14]});
+
+                    if ($this.options.frame)
+                        return animate($this);
+
+                });
+            }
         } else {
             this.serviceError('el_not_exist');
         }
@@ -55,21 +129,25 @@ var cherry = $c = {
         var end;
         /* check from*/
         if (typeof from != 'object' || from.length != 2) {
-            this.serviceError('params_format');return this;
+            this.serviceError('params_format');
+            return this;
         }
         /* check to */
         if (typeof to != 'object') {
-            this.serviceError('params_format');return this;
+            this.serviceError('params_format');
+            return this;
         } else {
             if (typeof to[0] == 'object') {
                 for (var i in to) {
                     if (to[i].length != 2) {
-                        this.serviceError('params_format');return this;
+                        this.serviceError('params_format');
+                        return this;
                     }
                 }
             } else {
                 if (to.length != 2) {
-                    this.serviceError('params_format');return this;
+                    this.serviceError('params_format');
+                    return this;
                 }
             }
         }
@@ -106,10 +184,10 @@ var cherry = $c = {
             line: function (to, options) {
                 return cherry.line(this.from, to, options !== 'undefined' ? options : this.options);
             },
-            remove: function(){
+            remove: function () {
                 return true;
             },
-            grad: function(){
+            grad: function () {
                 return cherry.grad();
             }
         };
@@ -118,26 +196,140 @@ var cherry = $c = {
     color: function (color) {
         this.canvas.fillStyle = color;
     },
-    grad: function(){
-        var grad = this.canvas.createLinearGradient(123,123,1,190);
+    grad: function () {
+        var grad = this.canvas.createLinearGradient(123, 123, 1, 190);
         grad.addColor(0.4, 'blue');
         return grad;
     },
-    
+    /* rect */
+    rect: function (x1, y1, x2, y2, options) {
+        var condition = {
+            fillStyle: this.canvas.fillStyle,
+            lineWidth: this.canvas.lineWidth,
+            strokeStyle: this.canvas.strokeStyle
+        }
+        this.canvas.beginPath();
+        this.canvas.rect(x1, y1, x2, y2);
+        if (typeof options == 'object') {
+            if (options.background != undefined) {
+                this.canvas.fillStyle = options.background;
+                this.canvas.fill();
+            }
+            if (options.width != undefined)
+                this.canvas.lineWidth = options.width;
+
+            if (options.line != undefined) {
+                this.canvas.strokeStyle = options.line;
+                this.canvas.stroke();
+            }
+
+        }
+        this.backCondition(condition);
+        return {
+            rect: function (x1, y1, x2, y2, options) {
+                return cherry.rect(x1, y1, x2, y2, options);
+            }
+        }
+    },
+    /* TEXT */
+    text: function (string, options) {
+        var condition = {
+            font: this.canvas.font,
+            fillStyle: this.canvas.fillStyle,
+            textAlign: this.canvas.textAlign,
+            textBaseline: this.canvas.textBaseline,
+        }
+        var x, y = 0, old_font = this.canvas.font;
+        if (typeof options == 'object') {
+            if (options.position != undefined && typeof options.position == 'object') {
+                x = options.position[0];
+                y = options.position[1];
+            }
+
+            if (options.font != undefined)
+                this.canvas.font = options.font;
+
+            if (options.color != undefined)
+                this.canvas.fillStyle = options.color;
+
+            if (options.align != undefined)
+                this.canvas.textAlign = options.align;
+            /* alphabetic, top, hanging, middle, ideographic, bottom*/
+            if (options.baseline != undefined)
+                this.canvas.textBaseline = options.baseline;
+
+        }
+        if (typeof string != 'string') {
+            this.serviceError('must_be_string');
+        } else {
+            if (options.stroke !== undefined && options.stroke)
+                this.canvas.strokeText(string, x, y);
+            else
+                this.canvas.fillText(string, x, y);
+        }
+        this.backCondition(condition);
+        return {
+            text: function (string, options) {
+                return cherry.text(string, options);
+            }
+        }
+
+    },
+    /* DRAW IMAGE */
+    image: function (img, options) {
+        if (typeof img != 'object') {
+            this.serviceError('image_not_found',{path:img});
+        } else {
+            if (options != undefined) {
+                if (options.length == 2) {
+                    this.canvas.drawImage(img, options[0], options[1]);
+                }
+                if (options.length == 4) {
+                    this.canvas.drawImage(img, options[0], options[1], options[2], options[3]);
+                }
+                if (options.length == 8) {
+
+                    cherry.canvas.drawImage(img, options[0], options[1], options[2], options[3],
+                            options[4], options[5], options[6], options[7]);
+
+                }
+            } else {
+                this.canvas.drawImage(img, 0, 0);
+            }
+
+        }
+
+        return {
+            image: function (options) {
+                return cherry.image(img, options);
+            }
+        };
+    },
+    /* back to last condition */
+    backCondition: function (condition) {
+        for (var i in condition) {
+            this.canvas[i] = condition[i];
+        }
+    },
     /**************************************************************************
      ********************SERVICE ERROR***************************************** 
      **************************************************************************/
     service_errors: [],
-    serviceError: function (key) {
+    serviceError: function (key, replaces) {
         if ($.serviceOptions.criticalError[key] != undefined)
         {
             var mess = 'Error:: ' + $.serviceOptions.criticalError[key] + '!!!';
+            if(replaces instanceof Object){
+                for(var key in replaces){
+                    mess = mess.replace("{"+key+"}", replaces[key]);
+                }
+            }
             this.service_errors.push(mess)
             return console.log(mess);
         } else {
             this.serviceError('bad_error_type');
         }
-    }
+    },
 }
 var is_debug = true;
 var p = function () {
